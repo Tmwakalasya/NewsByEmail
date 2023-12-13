46574c9891ad1a3eba6ef5b2dcfe6d8ddc8676c2
Tmwakalasya <mwakalasyatuntu@gmail.com>
2023-12-13 13:22:31 -0500

    Making API requests

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import requests
-api_key = "0615f778dcd74240a6bc51ea815ce976"
-# Endpoint is the actual url
-url = "https://newsapi.org/v2/everything?q=tesla&from=2023-05-11&sortBy=publishedAt&apiKey=0615f778dcd74240a6bc51ea815ce976"
-# Make a request
-requests = requests.get(url)
-# Get a dictionary with the data
-content = requests.json()
-# Access the article titles and description
-for article in content['articles']:
-    print(article['title'])
-    print(article['description'])
+import os
+from dotenv import load_dotenv
+load_dotenv()
+api_key = os.getenv("API_KEY")
+url = f"https://newsapi.org/v2/everything?q=tesla&from=2023-11-13&sortBy=publishedAt&apiKey={api_key}"
+request = requests.get(url)
+content = request.json()
+# print(len(content["articles"]))
+for article in content["articles"]:
+    print(article["title"])
